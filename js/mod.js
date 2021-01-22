92aba3f912827b0cfca9fdabffe54743ca59b15a
ace248952 <ace248952@yahoo.com>
2021-01-22 00:33:07 -0500

    Update mod.js
    1st tutorial test

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Chord Tree",
+	id: "minor7",
+	author: "ace248952",
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
